718114e2c4e92fd72ed3f66a74ee7b54af8a6dfe
OmkarRatna <90371805+OmkarRatna@users.noreply.github.com>
2023-02-20 23:13:33 +0530

    Delete matlab directory

--- matlab/compute_centroid.m
+++
@@ -1,8 +0,0 @@
-function [new_centroid]=compute_centroid(xt,index,k)
-new_centroid=zeros(1,k);
-for i=1:1:k
-	temp=find(index==i);
-	xtemp=xt(temp);
-	new_centroid(i)=((sum(xtemp))./length(xtemp));
-end
-end
